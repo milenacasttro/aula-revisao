c17910a47aec9155d272474551f70f35e59432c4
Yan M. Coutinho <yan.m.coutinho@gmail.com>
2024-06-11 10:07:44 -0300

    created a test for model

--- project/tests/controllers/UsersController.test.js
+++ project/tests/controllers/UsersController.test.js
@@ -18,5 +18,20 @@ describe('UsersController', () => {
     assert.strictEqual(substituicao1.calledOnceWith({name: USER.name, email: USER.email}), true);
     assert.strictEqual(RESPONSE.statusCode, 200);
     assert.deepStrictEqual(RESPONSE.body, {success: true});
+
+    substituicao1.restore();
   });
 });
+
+describe('User (model)', () => {
+  it('Deve criar o usuário com sucesso', async () => {
+    let user = await User.create(USER).fetch();
+
+    assert.strictEqual(user.id, USER.id)
+    assert.strictEqual(user.email, USER.email)
+    assert.strictEqual(user.name, USER.name)
+
+  });
+});
+
+
